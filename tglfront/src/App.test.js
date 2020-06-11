816939fb753fe89a9a8621cad43d84563a1934a2
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-06-11 11:02:18 +0100

    set up testing env

--- tglfront/src/App.test.js
+++ tglfront/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('checking jest/enzyme are working', () => {
+  const num = 5;
+  expect(2 + 3).toEqual(num);
+})
+
+describe('App', () => {
+  let wrapper = shallow(<App />);
+
+  it('shallow renders App', () => {
+    wrapper;
+  });
+
+  it('Should have 1 div', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  // write test cases of what component should
+  // render depending on the state values.
 });
+
+
+
