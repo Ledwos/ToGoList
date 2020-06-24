2baf5afc36d9cd2027f71430056555a054991e33
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-06-24 15:42:23 +0100

    build update

---
+++ tglfront/build/precache-manifest.a9113d24bec747f002e10ebdb1891cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bc88c187a8ec767427bdbfea212408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70b1bcc8f232217c2417",
+    "url": "/static/css/main.5fa07d30.chunk.css"
+  },
+  {
+    "revision": "c80ed6b236de8c6a8ac0",
+    "url": "/static/js/2.d187fec1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d187fec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b1bcc8f232217c2417",
+    "url": "/static/js/main.877fe2c6.chunk.js"
+  },
+  {
+    "revision": "d0d6e509f02cf09b8949",
+    "url": "/static/js/runtime-main.d398d15e.js"
+  }
+]);
